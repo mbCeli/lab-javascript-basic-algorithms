fb35f1431b93bda8457488305e709b12d16c9478
mbCeli <munbaporicelina@outlook.com>
2025-01-15 16:31:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (hacker1.length > hacker2.length) {
   console.log(
     `The driver has the longest name, it has ${hacker1.length} characters`
   );
-} else if (hacker2.length < hacker1.length) {
+} else if (hacker2.length > hacker1.length) {
   console.log(
     `It seems that the navigator has the longest name, it has ${hacker2.length} chracaters`
   );
